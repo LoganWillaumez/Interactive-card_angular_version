a192cad6805325d051290e36fd390c3d085dd946
LoganW <logan.willaumez@gmail.com>
2022-07-17 20:46:08 +0200

    [FEAT](map): add current position and label of position

--- src/app/intermap/map/map.component.ts
+++ src/app/intermap/map/map.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
+  zoom = 12;
+  center!: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: true,
+    scrollwheel: true,
+    disableDoubleClickZoom: true,
+    maxZoom: 50,
+    minZoom: 10,
+  };
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
+  }
 }
