fa5ca735daf7867b1681eb762c6087b78856e7c8
LoganW <logan.willaumez@gmail.com>
2022-07-17 12:59:31 +0200

    [FEAT](header-user): add navigation for mobile

--- src/app/header/burger-menu/burger-menu.component.ts
+++ src/app/header/burger-menu/burger-menu.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-burger-menu',
   templateUrl: './burger-menu.component.html',
-  styles: [],
+  styleUrls: ['./burger-menu.component.scss'],
 })
 export class BurgerMenuComponent implements OnInit {
+  @Input() burger = false;
+  @Input() toggleBurger: any;
   constructor() {}
 
   ngOnInit(): void {}
