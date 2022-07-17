74abf1a8da6bf3b39e7554c6b61ae7121d009321
LoganW <logan.willaumez@gmail.com>
2022-07-17 15:15:28 +0200

    [FEAT](contact): add contact page

--- src/app/intermap/intermap.module.ts
+++ src/app/intermap/intermap.module.ts
@@ -4,6 +4,7 @@ import { IntermapComponent } from './intermap.component';
 import { MapComponent } from './map/map.component';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: 'map', component: MapComponent },
@@ -12,7 +13,12 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [IntermapComponent, MapComponent, ContactComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   exports: [IntermapComponent],
 })
 export class IntermapModule {}
