1944fd74776836ac1f5918a3cee8aebcc497a7a3
LoganW <logan.willaumez@gmail.com>
2022-07-18 08:06:20 +0200

    [FEAT](env-security): add env.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { envSecret } from '../environments/env';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'technicalTest-angular';
+
+  ngOnInit() {
+    this.loadScript(
+      `https://maps.googleapis.com/maps/api/js?key=${envSecret.apiGoogle}`
+    ).then(() => {
+      console.log('Success');
+    });
+  }
+
+  loadScript(name: string) {
+    return new Promise<void>((resolve, reject) => {
+      let script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = name;
+      document.getElementsByTagName('head')[0].appendChild(script);
+      console.log('Script Loaded');
+      resolve();
+    });
+  }
 }
